6341c53d4767d62d9f3bfa142e12416df03d026e
Laura Murphy <laurahelenamurphy@gmail.com>
2019-07-14 21:50:48 +0100

    Toggle button working on background image only

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 $(document).ready(() => {
   console.log('this is working')
 
+  $('#navbar li').addClass('navbar-animate')
+
+  // $('.switch').click(function(){
+  //   if (!$('#profile-pic').hasClass('formal')) {
+  //     $('#profile-pic').attr('src', 'images/code.jpeg')
+  //     $('#profile-pic').addClass('formal')
+  //     $('#profile-pic').removeClass('casual')
+  //   } else  {
+  //     $('#profile-pic').attr('src', 'images/laura.png')
+  //     $('#profile-pic').addClass('casual')
+  //     $('#profile-pic').removeClass('formal')
+  //   }
+  // })
+
+  $('#checkbox').click(function () {
+    $('#profile-pic').toggleClass('casual')
+  })
+
   $('.about').click(function(event) {
     event.preventDefault()
-    $('html, body').animate({scrollTop: $('#about').offset().top-5}, 500)
+    $('html, body').animate({scrollTop: $('#about').offset().top-75}, 500)
   })
 
   $('.experience').click(function(event) {
     event.preventDefault()
-    $('html, body').animate({scrollTop: $('#experience').offset().top-5}, 500)
+    $('html, body').animate({scrollTop: $('#experience').offset().top-75}, 500)
   })
 
   $('.projects').click(function(event) {
     event.preventDefault()
-    $('html, body').animate({scrollTop: $('#projects').offset().top-5}, 500)
+    $('html, body').animate({scrollTop: $('#projects').offset().top-75}, 500)
   })
 
   $('.contact').click(function(event) {
