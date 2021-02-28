b5b1337ca2c24e98cd4dfa9277b1c725e8e426e0
Laura Murphy <laurahelenamurphy@gmail.com>
2021-02-28 12:20:03 +0000

    Storing redesign for now

--- app.js
+++ app.js
@@ -1,64 +1,97 @@
 $(document).ready(() => {
   console.log('this is working')
 
-  // $('.switch').click(function(){
-  //   if (!$('#profile-pic').hasClass('formal')) {
-  //     $('#profile-pic').attr('src', 'images/code.jpeg')
-  //     $('#profile-pic').addClass('formal')
-  //     $('#profile-pic').removeClass('casual')
-  //   } else  {
-  //     $('#profile-pic').attr('src', 'images/laura.png')
-  //     $('#profile-pic').addClass('casual')
-  //     $('#profile-pic').removeClass('formal')
+  // Counter for how long I've been coding:
+  var started = new Date(2019,8,1,00,00,00,0); 
+  var exactStart = started.getTime();
+  
+  var newDate = new Date();
+  var newStamp = newDate.getTime();
+  
+  var timer; 
+
+  let counterEl = document.querySelector('#counter');
+  
+  function updateClock() {
+      newDate = new Date();
+      newStamp = newDate.getTime();
+      var diff = Math.round((newStamp-exactStart)/1000);
+      
+      var d = Math.floor(diff/(24*60*60)); 
+      diff = diff-(d*24*60*60);
+      var h = Math.floor(diff/(60*60));
+      diff = diff-(h*60*60);
+      var m = Math.floor(diff/(60));
+      diff = diff-(m*60);
+      var s = diff;
+      
+      document.getElementById("counter-d").innerHTML = d + ` /&nbsp;`;
+      document.getElementById("counter-h").innerHTML = h + ` /&nbsp;`;
+      document.getElementById("counter-m").innerHTML = m + ` /&nbsp;`;
+      document.getElementById("counter-s").innerHTML = + s;
+  }
+  
+  timer = setInterval(updateClock, 1000);
+  // End of Counter
+
+  // Open modal to explainn counter:
+
+  const model = document.querySelector('.modal');
+  const cross = document.querySelector('.cross');
+  const body = document.querySelector('body');
+  let isOpen = false;
+
+  // body.addEventListener('click', function(evt) {
+  //   alert('clicking on body');
+  //   if(!$(evt.target).closest(model).lenght) {
+  //     if(isOpen === true) {
+  //       model.style.display = "none";
+  //     }
   //   }
   // })
+  // counterElClicked = getParentNodeElement(counterEl);
 
-  // $('.laura-button').on({
-  //   'click': function(){
-  //     $('#change-image').attr('src','images/laura.png')
-  //   }
-  // })
-  //
-  // $('.code-button').on({
-  //   'click': function(){
-  //     $('#change-image').attr('src','images/code.jpeg')
-  //   }
-  // })
 
-  var laura = 'images/laura.png'
-  var code = 'images/code.jpeg'
 
-  $('.switch').click(function() {
-    if ($('#change-image').attr('src') === code) {
-      $('#change-image').attr('src', laura)
-    } else {
-      $('#change-image').attr('src', code)
+  function openModal(evt) {
+    // counterEl = getParentNodeElement('DIV', evt.target)
+    counterEl = evt.target.closest('DIV');
+
+    if(evt.target === counterEl) {
+      model.style.display = "block";
+      isOpen = true;
     }
-  })
+  }
 
+  cross.addEventListener('click', function(evt) {
+    if(evt.target === cross) {
+      model.style.display = "none";
+      isOpen = false;
+    }
+  }) 
 
-  $('#checkbox').click(function () {
-    $('#profile-pic').toggleClass('casual')
-  })
+  function getParentNodeElement(nodeName, startNode) {
+    let found = startNode;
+    const stopNode = document.body;
+    
+      if(found.nodeName === nodeName) {
+        return found;
+      } 
+      found = startNode.parentElement;
+      
+      while(found && found !== stopNode) {
+        if(found.nodeName === nodeName) {
+          return found;
+        } 
+        found = found.parentElement;
+      }
+      if (found === stopNode) {
+        return null;
+      }
+  }
 
-  $('.about').click(function(event) {
-    event.preventDefault()
-    $('html, body').animate({scrollTop: $('#about').offset().top-75}, 500)
-  })
-
-  $('.experience').click(function(event) {
-    event.preventDefault()
-    $('html, body').animate({scrollTop: $('#experience').offset().top-75}, 500)
-  })
-
-  $('.projects').click(function(event) {
-    event.preventDefault()
-    $('html, body').animate({scrollTop: $('#projects').offset().top-75}, 500)
-  })
-
-  $('.contact').click(function(event) {
-    event.preventDefault()
-    $('html, body').animate({scrollTop: $('#contact').offset().top-0}, 500)
-  })
-
+  // add event listener to counterEl
+  counterEl.addEventListener('click', evt => openModal(evt, counterEl));
+  // evt = start
+  // counterEl  = stop
 })
